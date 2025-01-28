4d99f9987305930f27d223c99d36aaaf98280129
Agustin Diaz <adi0004@alu.medac.es>
2025-01-28 11:29:31 +0100

    react

---
+++ proyectoreact/nombreproyecto/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  function miFuncion(){
+    alert("Hola");
+  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <button type="button" onClick={miFuncion}>OK</button>
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
